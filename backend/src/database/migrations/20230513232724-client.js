7c5d24965bf358a531aa140066a7810a8e4e19c5
oligregz <gregorysak90@gmail.com>
2023-05-13 20:42:41 -0300

    feat: migratrion for client table implemented

--- backend/src/database/migrations/20230513232724-client.js
+++ backend/src/database/migrations/20230513232724-client.js
@@ -2,21 +2,34 @@
 
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
-  async up (queryInterface, Sequelize) {
-    /**
-     * Add altering commands here.
-     *
-     * Example:
-     * await queryInterface.createTable('users', { id: Sequelize.INTEGER });
-     */
+  up (queryInterface, Sequelize) {
+    return queryInterface.createTable('client_tb', {
+      id: {
+        type: Sequelize.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+        allowNull: false,
+      },
+      name: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      telephone: {
+        type: Sequelize.STRING,
+        allowNull: false
+      },
+      created_at: {
+        type: Sequelize.DATE,
+        allowNull: false
+      },
+      update_at: {
+        type: Sequelize.DATE,
+        allowNull: false
+      }
+    });
   },
 
-  async down (queryInterface, Sequelize) {
-    /**
-     * Add reverting commands here.
-     *
-     * Example:
-     * await queryInterface.dropTable('users');
-     */
+  down (queryInterface, Sequelize) {
+    return queryInterface.dropTable('client_tb');
   }
 };
