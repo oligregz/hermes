2957a079c881aadfc80f996b343bf68f57a64de3
oligregz <gregorysak90@gmail.com>
2023-05-13 16:50:29 -0300

    feat(backend): scope for implemented routes

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,8 +1,9 @@
 import  express from 'express';
+import routes from './routes/routes.js';
+
 const app = express();
 
-app.get('/', (req, res) => {
-  res.send('Hello World!');
-})
+app.use(express.json());
+app.use(routes);
 
 export default app;
