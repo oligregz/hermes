08832efb2657197030e6f1702634446e75caf61a
oligregz <gregorysak90@gmail.com>
2023-05-14 20:47:38 -0300

    feat(backend): reserve migration implemented

--- backend/src/database/migrations/20230513232537-reserve.js
+++ backend/src/database/migrations/20230513232537-reserve.js
@@ -2,21 +2,57 @@
 
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
-  async up (queryInterface, Sequelize) {
-    /**
-     * Add altering commands here.
-     *
-     * Example:
-     * await queryInterface.createTable('users', { id: Sequelize.INTEGER });
-     */
+  up (queryInterface, Sequelize) {
+    return queryInterface.createTable('reserve_tb', {
+      id: {
+        type: Sequelize.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+        allowNull: false,
+      },
+      date: {
+        type: Sequelize.DATEONLY,
+        allowNull: false,
+      },
+      hour: {
+        type: Sequelize.DATE,
+        allowNull: false
+      },
+      created_at: {
+        type: Sequelize.DATE,
+        allowNull: false
+      },
+      update_at: {
+        type: Sequelize.DATE,
+        allowNull: false
+      }
+    });
   },
 
-  async down (queryInterface, Sequelize) {
-    /**
-     * Add reverting commands here.
-     *
-     * Example:
-     * await queryInterface.dropTable('users');
-     */
+  down (queryInterface, Sequelize) {
+    return queryInterface.dropTable('reserve_tb');
   }
 };
+
+// clientId: {
+//   type: Sequelize.INTEGER,
+//   allowNull: false,
+//   onUpdate: 'CASCADE',
+//   onDelete: 'CASCADE',
+//   field: 'client_id',
+//   references: {
+//     model: 'client_tb',
+//     key: 'id',
+//   }
+// },
+// tableId: {
+//   type: Sequelize.INTEGER,
+//   allowNull: false,
+//   onUpdate: 'CASCADE',
+//   onDelete: 'CASCADE',
+//   field: 'table_id',
+//   references: {
+//     model: 'table_tb',
+//     key: 'id',
+//   }
+// },
