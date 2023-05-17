7f55215625b9b730738ba3d00779472dc4d5fb3f
oligregz <gregorysak90@gmail.com>
2023-05-17 08:35:49 -0300

    feat(frontend): components created and bootstrap implemented

---
+++ hermes-frontend/src/components/Button/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const Buntton = () => {
+  return (
+    <div>
+    </div>
+  )
+}
+
+export default Buntton;
---
+++ hermes-frontend/src/components/Forms/Register/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const FormRegister = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default FormRegister;
